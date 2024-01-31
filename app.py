056e3409f68280df3f038f97a38d0ab1628e7628
GeorgeBemrose <GeorgeBemrose@gmail.com>
2024-01-31 08:28:40 +0000

    modified:   app.py 	modified:   static/styles.css 	modified:   template/index.html

--- app.py
+++ app.py
@@ -12,20 +12,18 @@ def index():
     """Homepage"""
     if request.method == "POST":
         ingredient = request.form.get("ingredient")
-        recipe = lookup(ingredient)
+        recipeNameImage, recipe_dict, status_code = lookupRecipeNameImage(ingredient)
+        
         return render_template(
-            "index.html", recipe = recipe
+            "index.html", recipeNameImage = recipeNameImage, recipe_dict = recipe_dict, status_code = status_code
         )
     else:
 
         # TODO: Display the entries in the database on index.html
-        recipe = []
-        return render_template("index.html", recipe = recipe)
+        recipeNameImage = []
+        return render_template("index.html", recipeNameImage = recipeNameImage)
     
-def lookup(ingredient):
-    """Look up energy Gen"""
-    
-
+def lookupRecipeNameImage(ingredient):
         # Query API
     try:
         url = f'https://api.spoonacular.com/recipes/findByIngredients'
@@ -41,15 +39,35 @@ def lookup(ingredient):
         response = requests.get(url, params=params)
         if response.status_code == 200:
             data = response.json()
-            return data
-        return []
+            recipes_dict = {}
+            for recipe in data:
+                
+                recipes_dict[recipe['id']] = lookupDetail(recipe['id'])
+                
+            return data, recipes_dict, response.status_code
+        return [], [], response.status_code
         
     
     except (requests.RequestException, ValueError, KeyError, IndexError):
         return "Error"
 
+
+def lookupDetail(recipe_id):
+        # Query API
+    try:
+        url = f'https://api.spoonacular.com/recipes/{recipe_id}/information'
+
+        params = {
+            'apiKey': config.api_key
+        }
+
+        response = requests.get(url, params=params)
+        if response.status_code == 200:
+            data = response.json()
+            return data
+       
+        return []
+        
     
-
-
-
-
+    except (requests.RequestException, ValueError, KeyError, IndexError):
+        return "Error"
